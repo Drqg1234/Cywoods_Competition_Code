cd388a2543a0c9a10c7ed82c490ce17c5873402e
Drqg1234 <jonathan.rodriguez9921@gmail.com>
2022-11-15 08:56:53 -0600

    THIS IS A NEW THING

---
+++ src/AnnoyingCat.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class AnnoyingCat {
+    public static void main(String[] args) throws Exception {
+        System.out.println(" ,_     _\n" +
+                " |\\\\_,-~/\n" +
+                " / _  _ |   ,--.\n" +
+                "(  @  @ )  / ,-*\n" +
+                " \\  _T_/-._( (\n" +
+                " /         `. \\\n" +
+                "|          _ \\ |\n" +
+                " \\ \\ , /       |\n" +
+                "  || |-_\\__   /\n" +
+                " ((_/`(____,-*");
+    }
+}
